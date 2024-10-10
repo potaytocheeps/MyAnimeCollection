fc1182c86a939221028a108cd9c1c6088f3effa8
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2024-10-10 15:59:46 -0700

    Create file with SQL commands needed to create empty database

---
+++ my-anime-collection/schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS anime_shows;
+DROP TABLE IF EXISTS anime_releases;
+DROP TABLE IF EXISTS anime_collections;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE anime_shows (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    type TEXT NOT NULL
+);
+
+CREATE TABLE anime_releases (
+    id INTEGER NOT NULL,
+    anime_id INTEGER NOT NULL,
+    disc_type TEXT NOT NULL,
+    edition TEXT NOT NULL DEFAULT "Standard",
+    release_date TIMESTAMP NOT NULL,
+    FOREIGN KEY (anime_id) REFERENCES anime_shows (id)
+);
+
+CREATE TABLE anime_collections (
+    user_id INTEGER NOT NULL,
+    anime_id INTEGER NOT NULL,
+    release_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (anime_id) REFERENCES anime_shows(id),
+    FOREIGN KEY (release_id) REFERENCES anime_releases (id)
+);
