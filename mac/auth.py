d98f303ee90c0a7f66a104448a9ba9d3de9a714e
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2024-10-12 17:54:21 -0700

    Create register view for registering users in database

--- mac/auth.py
+++ mac/auth.py
@@ -12,3 +12,45 @@ from mac.db import get_database
 
 # Create "auth" blueprint for user authentication
 blueprint = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@blueprint.route("/register", methods=["GET", "POST"])
+def register():
+    """Registers users in database."""
+
+    # User reached this view via a POST request, by submitting a form
+    if request.method == "POST":
+        # Get user data submission
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Get connection to database
+        database = get_database()
+
+        # Keep track of any errors that may occur during user registration
+        error = None
+
+        # Ensure user submitted data
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+
+        # If user submitted data successfully, register the user in the database
+        if error is None:
+            try:
+                database.execute(
+                    "INSERT INTO users (username, password) VALUES (?, ?)",
+                    [username, generate_password_hash(password)]
+                )
+                database.commit()
+            except database.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        # Store any errors that may have occurred during registration
+        flash(error)
+
+    # Render registration form
+    return render_template("auth/register.html")
