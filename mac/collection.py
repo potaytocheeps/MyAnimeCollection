5d72d781159cb215d654e263d90125c0e1170d31
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2024-10-27 20:23:55 -0700

    Create index view and its html template

--- mac/collection.py
+++ mac/collection.py
@@ -7,3 +7,31 @@ from mac.auth import login_required
 from mac.db import get_database
 
 blueprint = Blueprint("collection", __name__)
+
+@blueprint.route("/")
+@login_required
+def index():
+    """Displays the user's anime collection."""
+
+    # Get connection to database
+    database = get_database()
+
+    # Get list of titles of shows that the user has added to their collection
+    collection = database.execute("SELECT title "
+                                  "FROM anime_shows "
+                                  "WHERE id in ("
+                                  "    SELECT anime_id"
+                                  "    FROM anime_collections"
+                                  "    WHERE user_id = ?"
+                                  ")",
+                                  [g.user["id"]]).fetchall()
+
+    # Will contain a message if user doesn't have a collection yet
+    message = None
+
+    # Add message if collection is empty
+    if not collection:
+        message = ("This is where your anime collection will be displayed once "
+                   "you add some shows to your collection!")
+
+    return render_template("collection/index.html", message=message, collection=collection)
